f312c7dfa2d4a48483f5a71e24716c5b06fcdd6f
takuya875 <57318943+takuya875@users.noreply.github.com>
2020-01-11 15:40:07 +0900

    sesrch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # gemにdeviseの記述をして、bundle installを実施している
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-    # 7つのアクションの定義を実施する。メッセージの一覧表示を行う&メッセージの入力ができるindexを記述する、create
-    # messagesというコントローラーを記述。only:[]で配列内で該当のアクションと7つの中のアクションを指定している
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
