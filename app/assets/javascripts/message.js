cf5ea7beaec09bc4fd3f4c3507258a5fb6aaef85
takuya875 <57318943+takuya875@users.noreply.github.com>
2020-01-19 15:29:15 +0900

    自動更新commit2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -82,7 +82,6 @@ $(function(){
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       //ルーティングで設定した通りのURLを指定
       url: "api/messages",
@@ -93,7 +92,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages)
       //追加するHTMLの入れ物を作る
         var insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
@@ -104,7 +102,7 @@ $(function(){
         });
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
